a4c1c6f53a76a130fa1666f888c7eeeac789f235
pathikrit <pathikritbhowmick@msn.com>
2023-11-01 14:26:16 -0400

    correct qty logic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from datetime import date, datetime
 from dateutil.relativedelta import relativedelta
+from dateutil import rrule
 
 import pandas as pd
 import streamlit as st
@@ -17,12 +18,6 @@ plan['target_cashflow'] = plan['target_monthly_cashflow'] * 12
 plan['cashflow'] = 0
 plan.set_index('year')
 
-def buy(security, qty):
-    cusip = security['cusip']
-    plan[f'cashflow_{cusip}'] = plan.apply(lambda row: 0 if row['year'] > security['maturity_date'].year else ((row['year'] == security['maturity_date'].year) + security['rate']) * security['redemption'] * qty, axis=1)
-    plan['cashflow'] += plan[f'cashflow_{security.cusip}']
-    securities.loc[securities['cusip'] == cusip, 'buy'] = qty
-
 securities = pd.concat([pd.read_csv(file) for file in FIDELITY_FIXED_INCOME_SEARCH_RESULTS])
 securities['cusip'] = securities['Cusip'].str.replace('="', '').str.replace('"', '')
 securities.set_index('cusip')
@@ -34,13 +29,34 @@ securities['maturity_date'] = pd.to_datetime(securities['Maturity Date'], format
 securities['buy'] = 0
 securities = securities.dropna(subset=['rate'])
 
-def find_best(end_date: date):
-    t = datetime.combine(end_date, datetime.max.time()) # TODO: remove this - we should just use date columns for maturity_date
+def buy(end_date: date):
+    t = datetime.combine(end_date, datetime.max.time())  # TODO: remove this - we should just use date columns for maturity_date
     investable = securities[(securities['maturity_date'] <= t) & (securities['maturity_date'] >= t - relativedelta(months=PAYOUT_MONTHS))]
-    return investable[investable['yield'] == investable['yield'].max()].sort_values(by=['maturity_date']).iloc[0]
+    security = investable[investable['yield'] == investable['yield'].max()].sort_values(by=['maturity_date']).iloc[0]
+    cusip = security['cusip']
+    cash_needed = 0
+    for dt in rrule.rrule(rrule.MONTHLY, dtstart=security['maturity_date'], until=end_date):
+        cash_needed += plan.loc[plan['year'] == dt.year].iloc[0]['target_monthly_cashflow']
+    qty = max(0, cash_needed//security['redemption'])
+    year = security['maturity_date'].year
+    plan[f'cashflow_{cusip}'] = plan.apply(lambda row: 0 if row['year'] > year else ((row['year'] == year) + security['rate']) * security['redemption'] * qty, axis=1)
+    plan['cashflow'] += plan[f'cashflow_{security.cusip}']
+    securities.loc[securities['cusip'] == cusip, 'buy'] = qty
 
-temp = find_best(date(2048, 12, 31))
-buy(temp, qty=10)
+# buy(temp)
+#
+# def run(end_date: date):
+#     if end_date.year < plan['year'].min():
+#         return
+#     security = find_best(end_date)
+#
+#
+#
+
+buy(date(2048, 12, 31))
+
+
+securities['amount'] = securities['price'] * securities['buy']
 
 class STREAMLIT_FORMATS(object):
     CURRENCY = '$%.0f'
@@ -48,7 +64,7 @@ class STREAMLIT_FORMATS(object):
     NUMBER = '%d'
 
 st.dataframe(
-    data=securities[['cusip', 'Coupon', 'price', 'Ask Yield to Worst', 'maturity_date', 'buy']].sort_values(by=['buy', 'maturity_date']),
+    data=securities[['cusip', 'Coupon', 'price', 'yield', 'maturity_date', 'buy', 'amount']].sort_values(by=['buy', 'yield', 'maturity_date'], ascending=False),
     column_config={
         'cusip': st.column_config.TextColumn(label='CUSIP'),
         'Coupon': st.column_config.NumberColumn(format=STREAMLIT_FORMATS.PERCENT),
@@ -56,15 +72,19 @@ st.dataframe(
         'price': st.column_config.NumberColumn(label='Price', format=STREAMLIT_FORMATS.CURRENCY),
         'yield': st.column_config.NumberColumn(label='Yield', format=STREAMLIT_FORMATS.PERCENT),
         'buy': st.column_config.NumberColumn(label='Buy', format=STREAMLIT_FORMATS.NUMBER),
-    }
+        'amount': st.column_config.NumberColumn(label='Amount', format=STREAMLIT_FORMATS.CURRENCY),
+    },
+    hide_index=True
 )
 
 st.dataframe(
     data=plan,
-    column_config={col: st.column_config.NumberColumn(format=STREAMLIT_FORMATS.CURRENCY if 'cashflow' in col else STREAMLIT_FORMATS.NUMBER) for col in plan.columns}
+    column_config={col: st.column_config.NumberColumn(format=STREAMLIT_FORMATS.CURRENCY if 'cashflow' in col else STREAMLIT_FORMATS.NUMBER) for col in plan.columns},
+    hide_index=True
 )
 
 ### TODO
 # 1. Print total amount needed to buy using st.metric
 # 2. Unit tests
+# 3. add function types
 ####
