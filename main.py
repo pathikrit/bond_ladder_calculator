002b6f7c248097d9eee27e93394b3ab0f3e07f1c
pathikrit <pathikritbhowmick@msn.com>
2023-11-01 13:36:13 -0400

    find best to buy

--- main.py
+++ main.py
@@ -18,9 +18,10 @@ plan['cashflow'] = 0
 plan.set_index('year')
 
 def buy(security, qty):
-    plan[f'cashflow_{security.cusip}'] = plan.apply(lambda row: 0 if row.year > security.maturity_date.year else ((row.year == security.maturity_date.year) + security.rate) * security.redemption * qty, axis=1)
+    cusip = security['cusip']
+    plan[f'cashflow_{cusip}'] = plan.apply(lambda row: 0 if row['year'] > security['maturity_date'].year else ((row['year'] == security['maturity_date'].year) + security['rate']) * security['redemption'] * qty, axis=1)
     plan['cashflow'] += plan[f'cashflow_{security.cusip}']
-    # security['qty_bought'] = qty
+    securities.loc[securities['cusip'] == cusip, 'buy'] = qty
 
 securities = pd.concat([pd.read_csv(file) for file in FIDELITY_FIXED_INCOME_SEARCH_RESULTS])
 securities['cusip'] = securities['Cusip'].str.replace('="', '').str.replace('"', '')
@@ -30,8 +31,16 @@ securities['price'] = securities['Price Ask']
 securities['redemption'] = 100
 securities['yield'] = securities['Ask Yield to Worst']
 securities['maturity_date'] = pd.to_datetime(securities['Maturity Date'], format='%m/%d/%Y')
-securities['qty_bought'] = 0
+securities['buy'] = 0
 securities = securities.dropna(subset=['rate'])
+
+def find_best(end_date: date):
+    t = datetime.combine(end_date, datetime.max.time()) # TODO: remove this - we should just use date columns for maturity_date
+    investable = securities[(securities['maturity_date'] <= t) & (securities['maturity_date'] >= t - relativedelta(months=PAYOUT_MONTHS))]
+    return investable[investable['yield'] == investable['yield'].max()].sort_values(by=['maturity_date']).iloc[0]
+
+temp = find_best(date(2048, 12, 31))
+buy(temp, qty=10)
 
 class STREAMLIT_FORMATS(object):
     CURRENCY = '$%.0f'
@@ -39,14 +48,14 @@ class STREAMLIT_FORMATS(object):
     NUMBER = '%d'
 
 st.dataframe(
-    data=securities[['cusip', 'Coupon', 'price', 'Ask Yield to Worst', 'maturity_date', 'qty_bought']],
+    data=securities[['cusip', 'Coupon', 'price', 'Ask Yield to Worst', 'maturity_date', 'buy']].sort_values(by=['buy', 'maturity_date']),
     column_config={
         'cusip': st.column_config.TextColumn(label='CUSIP'),
         'Coupon': st.column_config.NumberColumn(format=STREAMLIT_FORMATS.PERCENT),
         'maturity_date': st.column_config.DateColumn(label='Maturity', format='YYYY-MM-DD'),
         'price': st.column_config.NumberColumn(label='Price', format=STREAMLIT_FORMATS.CURRENCY),
         'yield': st.column_config.NumberColumn(label='Yield', format=STREAMLIT_FORMATS.PERCENT),
-        'qty_bought': st.column_config.NumberColumn(label='Buy', format=STREAMLIT_FORMATS.NUMBER),
+        'buy': st.column_config.NumberColumn(label='Buy', format=STREAMLIT_FORMATS.NUMBER),
     }
 )
 
