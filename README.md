f7f4421c5a848b8f0887758fc2cc729310080156
pathikrit <pathikritbhowmick@msn.com>
2023-11-01 10:37:07 -0400

    streamlit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Fixed Income to Annuity
 ----
 
-* Constructs an Annuity out of Fixed Income securities
+* Constructs an [annuity](https://www.investopedia.com/investing/overview-of-annuities/) with custom cashflow out of [fixed income, bonds and CDs](https://fixedincome.fidelity.com/ftgw/fi/FILanding)
--- main.py
+++ main.py
@@ -1,18 +1,37 @@
-from datetime import datetime, date
 import pandas as pd
+import streamlit as st
 
-def bond_cashflow(rate: float, price: float, maturity: datetime, settlement: datetime = date.today(), redemption: float = 100, cusip: str = None):
-    df = pd.DataFrame(data={'year': range(settlement.year, maturity.year + 1)})
-    df['cashflow'] = df.apply(lambda row: rate * redemption + (0 if row['year'] < maturity.year else redemption), axis=1)
-    df[f'cashflow_{cusip}'] = df.apply(lambda row: rate * redemption + (0 if row['year'] < maturity.year else redemption), axis=1)
-    return df
+#### This is the input to this program ####
 
-if __name__ == '__main__':
-    securities = pd.read_csv('~/Downloads/Fidelity_FixedIncome_SearchResults.csv')
-    securities['cusip'] = securities['Cusip'].str.replace('="', '').str.replace('"', '')
-    securities['rate'] = securities['Coupon']/100
-    securities['price'] = securities['Price Ask']
-    securities['maturity_date'] = pd.to_datetime(securities['Maturity Date'], format='%m/%d/%Y')
+TARGET_CASHFLOW = [(year, 48000 - (2039 - year) * 1000 if year < 2040 else 51000 - (2048 - year) * 1000) for year in range(2025, 2049)]
+FIDELITY_FIXED_INCOME_SEARCH_RESULTS = '~/Downloads/Fidelity_FixedIncome_SearchResults.csv'
 
-    security = securities.iloc[467]
-    print(bond_cashflow(rate=security['rate'], price=security['price'], maturity=security['maturity_date'], cusip=security['cusip']))
+###########################################
+
+plan = pd.DataFrame(data=TARGET_CASHFLOW, columns=['year', 'target_monthly_cashflow'])
+plan['target_cashflow'] = plan['target_monthly_cashflow'] * 12
+plan['cashflow'] = 0
+plan.set_index('year')
+
+def buy(security, qty):
+    plan[f'cashflow_{security.cusip}'] = plan.apply(lambda row: 0 if row.year > security.maturity_date.year else ((row.year == security.maturity_date.year) + security.rate) * security.redemption * qty, axis=1)
+    plan['cashflow'] += plan[f'cashflow_{security.cusip}']
+    # security['qty_bought'] = qty
+
+
+securities = pd.read_csv(FIDELITY_FIXED_INCOME_SEARCH_RESULTS)
+securities['cusip'] = securities['Cusip'].str.replace('="', '').str.replace('"', '')
+securities.set_index('cusip')
+securities['rate'] = securities['Coupon'] / 100
+securities['price'] = securities['Price Ask']
+securities['redemption'] = 100
+securities['maturity_date'] = pd.to_datetime(securities['Maturity Date'], format='%m/%d/%Y')
+securities['qty_bought'] = 0
+securities = securities.dropna(subset=['rate'])
+securities = securities.sort_values(by=['maturity_date'], ascending=False)
+
+buy(securities.iloc[46], qty=10)
+print(plan)
+
+st.write(plan)
+st.write(securities[['cusip', 'rate', 'price', 'maturity_date', 'qty_bought']])
