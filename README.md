bef5d5418437e143895aa0f1f4490dd980a727bb
pathikrit <pathikritbhowmick@msn.com>
2023-11-02 21:40:55 -0400

    variable names

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Fixed Income to Annuity
 ----
 
-* Constructs an [annuity](https://www.investopedia.com/investing/overview-of-annuities/) with custom cashflow out of [fixed income, bonds and CDs](https://fixedincome.fidelity.com/ftgw/fi/FILanding)
+* Constructs an [annuity](https://www.investopedia.com/investing/overview-of-annuities/) with custom cashflow out of [bonds](https://fixedincome.fidelity.com/ftgw/fi/FILanding#tbindividual-bonds|treasury)
 
 Running this code:
 
--- main.py
+++ main.py
@@ -7,8 +7,7 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-######### INPUTS ###########
-
+################ INPUTS ##############
 TARGET_MONTHLY_CASHFLOW_BY_YEAR = {
     2025: 34000,
     2026: 35000,
@@ -46,8 +45,9 @@ CASH_OUT_APR = 1.0 / 100  # APR if we simply hold cash
 plan = pd.DataFrame(data=list(TARGET_MONTHLY_CASHFLOW_BY_YEAR.items()), columns=['year', 'target_monthly_cashflow'])
 plan['target_cashflow'] = plan['target_monthly_cashflow'] * 12
 plan['cashflow'] = 0
+START_DATE = date(year=plan['year'].min(), month=1, day=1)
+END_DATE = date(year=plan['year'].max(), month=12, day=31)
 plan = plan.set_index('year')
-START_DATE = date(plan.index.min(), 1, 1)
 
 securities = pd.concat([pd.read_csv(file) for file in FIDELITY_FIXED_INCOME_SEARCH_RESULTS])
 securities['cusip'] = securities['Cusip'].str.replace('="', '').str.replace('"', '')
@@ -62,42 +62,44 @@ securities = securities.dropna(subset=['rate'])
 securities = securities[securities['Attributes'].str.contains('CP')]  # Non-callable bonds only
 
 
-def buy(end_date: date):
-    if end_date < START_DATE:
+def buy(max_maturity_date: date):  # TODO: add tests
+    if max_maturity_date < START_DATE:
         return
 
     def cash_adjusted_yield(row):
-        months_in_between = end_date.month - row['maturity_date'].month + 12 * (end_date.year - row['maturity_date'].year)
+        months_in_between = max_maturity_date.month - row['maturity_date'].month + 12 * (max_maturity_date.year - row['maturity_date'].year)
         return 0 if months_in_between <= 0 else row['yield'] / 100 - months_in_between * CASH_OUT_APR / 12
 
     securities['cash_adjusted_yield'] = securities.apply(cash_adjusted_yield, axis=1)
-    security = securities[securities['cash_adjusted_yield'] == securities['cash_adjusted_yield'].max()] \
-        .sort_values(by=['maturity_date'], ascending=False).iloc[0]
+    security = securities[securities['cash_adjusted_yield'] == securities['cash_adjusted_yield'].max()].iloc[0]
     cusip = security.name
+    maturity_date = security['maturity_date'].date()
 
-    logging.debug(f"Found CUSIP={cusip} for maturity={security['maturity_date']} to cover until end_date={end_date}")
+    logging.debug(f"Found CUSIP={cusip} with maturity_date={maturity_date} to cover until end_date={max_maturity_date}")
     plan[f'cashflow_{cusip}'] = 0.0
 
     cash_needed_at_maturity = 0
-    for date in reversed(list(rrule.rrule(rrule.MONTHLY, dtstart=security['maturity_date'].replace(day=1), until=end_date))):
-        if not date.year in plan.index:
+    for dt in reversed(list(rrule.rrule(rrule.MONTHLY, dtstart=maturity_date.replace(day=1), until=max_maturity_date))):
+        if not dt.year in plan.index:
             continue
-        cash_needed_for_this_month = plan.loc[date.year, 'target_cashflow'] / date.month
-        plan.loc[date.year, 'target_cashflow'] -= cash_needed_for_this_month
-        plan.loc[date.year, f'cashflow_{cusip}'] += cash_needed_for_this_month
+        cash_needed_for_this_month = plan.loc[dt.year, 'target_cashflow'] / dt.month
+        plan.loc[dt.year, 'target_cashflow'] -= cash_needed_for_this_month
+        plan.loc[dt.year, f'cashflow_{cusip}'] += cash_needed_for_this_month
         cash_needed_at_maturity += cash_needed_for_this_month
-        logging.debug(f"\tCash needed for {date} = {cash_needed_for_this_month}")
+        logging.debug(f"\tCash needed for {dt} = {cash_needed_for_this_month}")
 
     quantity = max(0, cash_needed_at_maturity) // security['redemption']
     securities.loc[cusip, 'buy'] = quantity
 
-    for date in rrule.rrule(rrule.YEARLY, dtstart=START_DATE, until=security['maturity_date']):
-        dividend = security['rate'] * security['redemption'] * quantity
-        plan.loc[date.year, f'cashflow_{cusip}'] += dividend
-        plan.loc[date.year, 'target_cashflow'] -= dividend
+    if security['rate'] > 0:  # if this pays dividends
+        for dt in rrule.rrule(rrule.YEARLY, dtstart=START_DATE, until=maturity_date):
+            dividend = security['rate'] * security['redemption'] * quantity
+            plan.loc[dt.year, f'cashflow_{cusip}'] += dividend
+            plan.loc[dt.year, 'target_cashflow'] -= dividend
+        plan['target_cashflow'] = plan['target_cashflow'].clip(lower=0)  # sometimes dividend payout may exceed our needs by a bit
 
     plan['cashflow'] += plan[f'cashflow_{cusip}']
-    buy(security['maturity_date'].date() - relativedelta(days=1))
+    buy(max_maturity_date=maturity_date - relativedelta(days=1))  # buy next security with max maturity date 1 day before this one matures
 
 
 class Styles:
@@ -123,15 +125,12 @@ class Styles:
 
 
 if __name__ == "__main__":
-    buy(date(plan.index.max(), 12, 31))
+    buy(max_maturity_date=END_DATE)
     securities['amount'] = securities['price'] * securities['buy']
     plan['target_cashflow'] = plan['target_monthly_cashflow'] * 12
 
-    st.metric(
-        label='Total Investment',
-        value=Styles.money().format(securities['amount'].sum())
-    )
-
+    st.metric(label='Total Investment', value=Styles.money().format(securities['amount'].sum()))
+    st.metric(label='Total Payout', value=Styles.money().format(plan['cashflow'].sum()))
     st.dataframe(
         data=securities[['Coupon', 'price', 'yield', 'maturity_date', 'buy', 'amount', 'Description']]
         .rename(columns=str.lower)
@@ -148,7 +147,6 @@ if __name__ == "__main__":
             'description': Styles.string()
         })
     )
-
     st.dataframe(
         data=plan
         .replace(0, np.nan)
@@ -157,5 +155,3 @@ if __name__ == "__main__":
             '_index': st.column_config.NumberColumn(format='%d')
         }
     )
-
-# TODO: add tests
