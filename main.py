e61fabe859ade114cf74cccadc188911f8704d69
pathikrit <pathikritbhowmick@msn.com>
2023-11-29 12:09:46 -0500

    swap sample input

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from datetime import date
 from typing import List, Dict
+import textwrap
 
 import logging
 from dateutil.relativedelta import relativedelta
@@ -208,7 +209,32 @@ def main():
         'tests/fidelity_downloads/TREASURY_2023-11-02.csv'
     ])
     target_monthly_cashflow_by_year = st_ace(
-        value='{year: 35000 + 250*i for i, year in enumerate(range(2026, 2049))}',
+        value=textwrap.dedent("""{
+            2025: 33000,
+            2026: 33500,
+            2027: 34000,
+            2028: 34500,
+            2029: 35000,
+            2030: 35500,
+            2031: 36000,
+            2032: 36500,
+            2033: 37000,
+            2034: 37500,
+            2035: 38000,
+            2036: 38500,
+            2037: 39000,
+            2038: 39500,
+            2039: 40000,
+            2040: 33000,
+            2041: 33500,
+            2042: 34000,
+            2043: 34500,
+            2044: 35000,
+            2045: 35500,
+            2046: 36000,
+            2047: 36500,
+            2048: 37000
+        }"""),
         language='python',
         theme='terminal',
         auto_update=True,
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -9,31 +9,6 @@ class TestCalculator(TestCase):
             'tests/fidelity_downloads/CD_2023-11-02.csv',
             'tests/fidelity_downloads/TREASURY_2023-11-02.csv'
         ])
-        result = calculator.calculate(target_monthly_cashflow_by_year={
-            2025: 33000,
-            2026: 33500,
-            2027: 34000,
-            2028: 34500,
-            2029: 35000,
-            2030: 35500,
-            2031: 36000,
-            2032: 36500,
-            2033: 37000,
-            2034: 37500,
-            2035: 38000,
-            2036: 38500,
-            2037: 39000,
-            2038: 39500,
-            2039: 40000,
-            2040: 33000,
-            2041: 33500,
-            2042: 34000,
-            2043: 34500,
-            2044: 35000,
-            2045: 35500,
-            2046: 36000,
-            2047: 36500,
-            2048: 37000
-        })
-        self.assertEqual(int(result.total_investment), 5605153)
-        self.assertEqual(int(result.total_cashflow), 10350420)
+        result = calculator.calculate(target_monthly_cashflow_by_year={year: 30000 + 500*i for i, year in enumerate(range(2025, 2050))})
+        self.assertEqual(int(result.total_investment), 5542923)
+        self.assertEqual(int(result.total_cashflow), 10800387)
